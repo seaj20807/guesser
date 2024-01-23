5a87c701d142a087b832c11ddb9bb7ae24353cfd
Sampo Jaakkola <sampo.jaakkola@myy.haaga-helia.fi>
2024-01-23 23:03:18 +0200

    Guesser app to git

--- App.js
+++ App.js
@@ -1,20 +1,49 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { useState } from "react";
+import { StyleSheet, Text, TextInput, Button, View, Alert } from "react-native";
 
 export default function App() {
+  const [number, setNumber] = useState("");
+  const [answer, setAnswer] = useState(Math.floor(Math.random() * 100) + 1);
+  const [tries, setTries] = useState(1);
+  const [text, setText] = useState("Guess a number between 1-100");
+
+  const evaluate = () => {
+    setTries(tries + 1);
+    if (number == answer) {
+      Alert.alert("You guessed the number in " + tries + " guesses");
+    } else if (number < answer) {
+      setText("Your guess of " + number + " is too low");
+    } else if (number > answer) {
+      setText("Your guess of " + number + " is too high");
+    }
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>{text}</Text>
+      <TextInput
+        style={styles.input}
+        onChangeText={(number) => setNumber(number)}
+        value={number}
+        keyboardType="numeric"
+      />
+      <Button title="Guess" onPress={evaluate} />
       <StatusBar style="auto" />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
+  input: {
+    width: 200,
+    borderColor: "grey",
+    borderWidth: 1,
+  },
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
